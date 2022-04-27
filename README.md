21cb26411b1c7dabc1f159da9301b34267492b28
rink1969 <rink1969@gmail.com>
2022-04-27 14:42:50 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
 # consensus_overlord
+`CITA-Cloud`中[consensus微服务](https://github.com/cita-cloud/cita_cloud_proto/blob/master/protos/consensus.proto)的实现。
+
+基于[overlord算法](https://github.com/nervosnetwork/overlord)实现。
+
+## 编译docker镜像
+```
+docker build -t citacloud/consensus_overlord .
+```
+
+## 使用方法
+
+```
+$ consensus -h       
+consensus 6.4.0
+Rivtower Technologies <contact@rivtower.com>
+This doc string acts as a help message when the user runs '--help' as do all doc strings on fields
+
+USAGE:
+    consensus <SUBCOMMAND>
+
+OPTIONS:
+    -h, --help       Print help information
+    -V, --version    Print version information
+
+SUBCOMMANDS:
+    help    Print this message or the help of the given subcommand(s)
+    run     run this service
+```
+
+### consensus-run
+
+运行`consensus`服务。
+
+```
+$ consensus run -h
+consensus-run 
+run this service
+
+USAGE:
+    consensus run [OPTIONS]
+
+OPTIONS:
+    -c, --config <CONFIG_PATH>    Chain config path [default: config.toml]
+    -h, --help                    Print help information
+    -l, --log <LOG_FILE>          log config path [default: consensus-log4rs.yaml]
+
+```
+
+参数：
+1. 微服务配置文件。
+
+   参见示例`example/config.toml`。
+
+   其中：
+    * `consensus_port` 为该服务监听的端口号。
+2. 日志配置文件。
+
+   参见示例`consensus-log4rs.yaml`。
+
+   其中：
+
+    * `level` 为日志等级。可选项有：`Error`，`Warn`，`Info`，`Debug`，`Trace`，默认为`Info`。
+    * `appenders` 为输出选项，类型为一个数组。可选项有：标准输出(`stdout`)和滚动的日志文件（`journey-service`），默认为同时输出到两个地方。
+
+```
+$ consensus run -c example/config.toml -l consensus-log4rs.yaml
+2022-04-28T02:33:22.839993245+00:00 INFO consensus - start consensus overlord
+2022-04-28T02:33:22.840116114+00:00 INFO consensus - grpc port of this service: 50001
+```
---
+++ src/util.rs
@@ -0,0 +1,62 @@
+// Copyright Rivtower Technologies LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::config::ConsensusConfig;
+use cita_cloud_proto::controller::consensus2_controller_service_client::Consensus2ControllerServiceClient;
+use cita_cloud_proto::kms::kms_service_client::KmsServiceClient;
+use cita_cloud_proto::network::network_service_client::NetworkServiceClient;
+use tokio::sync::OnceCell;
+use tonic::transport::{Channel, Endpoint};
+
+pub static KMS_CLIENT: OnceCell<KmsServiceClient<Channel>> = OnceCell::const_new();
+pub static NETWORK_CLIENT: OnceCell<NetworkServiceClient<Channel>> = OnceCell::const_new();
+pub static CONTROLLER_CLIENT: OnceCell<Consensus2ControllerServiceClient<Channel>> =
+    OnceCell::const_new();
+
+// This must be called before access to clients.
+pub fn init_grpc_client(config: &ConsensusConfig) {
+    KMS_CLIENT
+        .set({
+            let addr = format!("http://127.0.0.1:{}", config.kms_port);
+            let channel = Endpoint::from_shared(addr).unwrap().connect_lazy().unwrap();
+            KmsServiceClient::new(channel)
+        })
+        .unwrap();
+    NETWORK_CLIENT
+        .set({
+            let addr = format!("http://127.0.0.1:{}", config.network_port);
+            let channel = Endpoint::from_shared(addr).unwrap().connect_lazy().unwrap();
+            NetworkServiceClient::new(channel)
+        })
+        .unwrap();
+    CONTROLLER_CLIENT
+        .set({
+            let addr = format!("http://127.0.0.1:{}", config.controller_port);
+            let channel = Endpoint::from_shared(addr).unwrap().connect_lazy().unwrap();
+            Consensus2ControllerServiceClient::new(channel)
+        })
+        .unwrap();
+}
+
+pub fn kms_client() -> KmsServiceClient<Channel> {
+    KMS_CLIENT.get().cloned().unwrap()
+}
+
+pub fn network_client() -> NetworkServiceClient<Channel> {
+    NETWORK_CLIENT.get().cloned().unwrap()
+}
+
+pub fn controller_client() -> Consensus2ControllerServiceClient<Channel> {
+    CONTROLLER_CLIENT.get().cloned().unwrap()
+}
